6965d8c1059dbc1650ec63bd8ca2a4b114cd7396
Muhammad Farid Mauludin <muhammadfaridmauludin1@gmail.com>
2025-07-09 13:05:27 +0700

    perbaikan eror

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -6,7 +6,7 @@ use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Queue\Middleware\RateLimited;
-use Illuminate\Support\Facades\Ratelimiter;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
